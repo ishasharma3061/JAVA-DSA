af49f82ac8c5ed4996d83543f4cf71147bc638e5
Isha Sharma <ishasharma3061@gmail.com>
2026-02-09 18:57:04 +0530

    Add files via upload

---
+++ secondLargestSmallest.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class secondLargestSmallest {
+   public static void main(String[] args) {
+    System.out.println("enter the number:");
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+    int[] arr={2,4,5,5,6,7,8,9};
+    for(int i=0;i)
+   } 
+}
